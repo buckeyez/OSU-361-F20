b8de1b6ddb278e115be8cb96198fa51702d8b1b2
Gagandeep Singh <10890654+Solid-Snek@users.noreply.github.com>
2020-11-22 17:40:46 -0600

    Reorganization

--- HTML-To-Convert/expertProfilegithub.js
+++ HTML-To-Convert/expertProfilegithub.js
@@ -31,23 +31,24 @@ function gitHubPopup(username)
         var repoP = document.createElement("p"); 
         var repoName = document.createTextNode("Repo Name: " + jsonResponse[i].name + "\n");
 
-        var repoDescP = document.createElement("p"); 
-        var repoDescription = document.createTextNode("Description: " + jsonResponse[i].description + " ");
-
-        var repoUpdatedP = document.createElement("p"); 
-        var repoUpdatedTS = document.createTextNode("Last Updated: " + jsonResponse[i].updated_at + " ");
-
         repoP.appendChild(repoName)
         popUp.appendChild(repoP);
 
+        var repoDescP = document.createElement("p"); 
+        var repoDescription = document.createTextNode("Description: " + jsonResponse[i].description + " ");
+
         repoDescP.appendChild(repoDescription)
         popUp.appendChild(repoDescP);
+
+        var repoUpdatedP = document.createElement("p"); 
+        var repoUpdatedTS = document.createTextNode("Last Updated: " + jsonResponse[i].updated_at + " ");
 
         repoUpdatedP.appendChild(repoUpdatedTS)
         popUp.appendChild(repoUpdatedP);
 
         var newLink = document.createElement('a');
         newLink.href = jsonResponse[i].html_url;
+        
         var linkTextNode = document.createTextNode(jsonResponse[i].full_name)
         newLink.appendChild(linkTextNode);
         popUp.appendChild(newLink);
